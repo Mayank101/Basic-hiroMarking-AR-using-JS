66d17dad29f6802bcb57b492ed5e4acda96de1d3
Mayank Kakkar <mayank@Mayanks-MacBook-Air.local>
2019-12-04 11:42:53 +0530

    Init commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile('/public/index.html', {root: __dirname });
+});
+
+
+app.listen(port, ()=>{
+    console.log('Server is up on port '+ port)
+});
