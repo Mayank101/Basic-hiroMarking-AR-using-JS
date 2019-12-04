0642f9e2bfe1225f819c6c4cb910afb52cab9b3f
Mayank Kakkar <mayank@Mayanks-MacBook-Air.local>
2019-12-04 12:27:07 +0530

    third commit

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const app = express()
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
+// app.get('/',(req,res)=>{
+//     res.sendFile('/public/index.html', {root: __dirname });
+// });
 app.get('/',(req,res)=>{
-    res.sendFile('/public/index.html', {root: __dirname });
+    res.sendFile('/public/sample.html', {root: __dirname });
 });
 
 
